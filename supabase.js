cc1a4d1354e2ee804a912af499a1604946e5bcbb
Sam <dhuy2791@gmail.com>
2026-08-14 13:39:57 +0700

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://opeyxasbcsfxvrpavcud.supabase.co";
+const SUPABASE_KEY = "sb_publishable_e2PM4XZPl8jrr-ByEbGNFA_3-Q18A8d";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
